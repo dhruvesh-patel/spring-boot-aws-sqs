056f345bb32da1d3b256f22726d50d5a0c01c9a1
Dhruvesh Patel <dmpatel.1412@gmail.com>
2020-09-16 21:23:54 +0100

    Added spring boot project with required dependancies

---
+++ spring-boot-aws-sqs/src/main/java/com/dpins/springbootawssqs/SpringBootAwsSqsApplication.java
@@ -0,0 +1,13 @@
+package com.dpins.springbootawssqs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAwsSqsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAwsSqsApplication.class, args);
+	}
+
+}
---
+++ spring-boot-aws-sqs/src/test/java/com/dpins/springbootawssqs/SpringBootAwsSqsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dpins.springbootawssqs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAwsSqsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
